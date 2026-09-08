610eddc4bd1355a3385242e1899d23b28883030d
Nil SparkLab <pan.india.1010@gmail.com>
2026-09-08 15:56:18 +0530

    Add files via upload

---
+++ js/config/app-config.js
@@ -0,0 +1,5 @@
+/* NIL SparkLab application configuration — extracted Stage 12 module. */
+(function(){
+  'use strict';
+  window.ElectroLabConfig = Object.freeze({ version: "11.0", projectFormat: "11.0" });
+})();
