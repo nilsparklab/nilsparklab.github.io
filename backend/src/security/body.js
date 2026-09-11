40601a941d3b9d2af7bd1ee22fa71b8da2aac98c
Nil SparkLab <pan.india.1010@gmail.com>
2026-09-12 00:13:32 +0530

    Delete backend/src/security directory

--- backend/src/security/body.js
+++
@@ -1,6 +0,0 @@
-export async function readJson(req,maxBytes=32*1024){
-  let size=0, chunks=[];
-  for await (const chunk of req){ size+=chunk.length; if(size>maxBytes) throw new Error('BODY_TOO_LARGE'); chunks.push(chunk); }
-  if(!chunks.length) return {};
-  try{return JSON.parse(Buffer.concat(chunks).toString('utf8'));}catch{throw new Error('INVALID_JSON');}
-}
