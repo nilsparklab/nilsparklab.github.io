a23bc02c327b84eec2054901643082c00a21b8ca
Nil SparkLab <pan.india.1010@gmail.com>
2026-09-12 00:25:29 +0530

    Delete js/config directory

--- js/config/app-config.js
+++
@@ -1,5 +0,0 @@
-/* NIL SparkLab application configuration — extracted Stage 12 module. */
-(function(){
-  'use strict';
-  window.ElectroLabConfig = Object.freeze({ version: "11.0", projectFormat: "11.0" });
-})();
