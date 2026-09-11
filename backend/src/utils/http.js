4241d5cd51013bc6eff31d3a637949b0ca62ac2c
Nil SparkLab <pan.india.1010@gmail.com>
2026-09-12 00:15:46 +0530

    Delete backend/src/utils/http.js

--- backend/src/utils/http.js
+++
@@ -1,5 +0,0 @@
-export function json(res,status,body,headers={}){
-  res.writeHead(status,{ 'content-type':'application/json; charset=utf-8','cache-control':'no-store',...headers });
-  res.end(JSON.stringify(body));
-}
-export function requestId(){ return crypto.randomUUID(); }
